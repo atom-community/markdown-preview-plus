2136a3efdd131006485b2b8dd831b5678d547ee8
Lukas Eipert <git@leipert.io>
2015-09-18 19:16:20 +0200

    Adjusted appveyor

--- assets/appveyor.ps1
+++ assets/appveyor.ps1
@@ -1,4 +1,5 @@
-$OutputVariable = (apm test --no-color --one) | Out-String
+Write-Host $PSVersionTable.PSVersion
+$OutputVariable = (apm test --no-color --one --path $env:LOCALAPPDATA\Atom\resources\cli\atom.cmd) | Out-String
 Write-Host $OutputVariable
 if($OutputVariable.contains(", 0 failures,")){
   Write-Host "All fine"
