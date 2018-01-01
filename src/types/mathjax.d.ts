f84396f771eae1e5d656acb3d9c4108be79af96e
Nikolay Yakimov <root@livid.pp.ru>
2018-01-02 02:23:38 +0300

    Add MathJax typing

--- src/types/mathjax.d.ts
+++ src/types/mathjax.d.ts
@@ -1,20 +1,9 @@
-type MathJaxQueueElement =
-  | ['Typeset', MathJaxHub, Node | Node[]]
-  | ['setRenderer', MathJaxHub, 'SVG' | 'HTML-CSS']
-  | [() => void]
-
-interface MathJaxHub {
-  Queue(...args: MathJaxQueueElement[]): void
-  Configured(): void
-  Config(cfg: {}): void
-}
-
-interface MathJaxInterface {
-  Hub: MathJaxHub
-}
-
 interface Window {
-  MathJax?: MathJaxInterface
+  MathJax?: typeof MathJax
 }
 
-declare const MathJax: MathJaxInterface | undefined
+declare namespace MathJax {
+  interface Hub {
+    Queue(...args: any[]): void
+  }
+}
