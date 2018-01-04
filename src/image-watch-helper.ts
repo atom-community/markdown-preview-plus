ac6c33c21f9b20c3988c4dbc5e222fbbcc4f6e6b
Nikolay Yakimov <root@livid.pp.ru>
2018-01-04 08:19:34 +0300

    Revert to old pathwatcher code

--- src/image-watch-helper.ts
+++ src/image-watch-helper.ts
@@ -1,7 +1,25 @@
 import fs = require('fs-plus')
 import _ = require('lodash')
 import { isMarkdownPreviewView } from './cast'
-import { watchPath, FilesystemChangeEvent, PathWatcher } from 'atom'
+// import { watchPath, FilesystemChangeEvent, PathWatcher } from 'atom'
+
+declare module 'pathwatcher' {
+  export function watch(
+    filename: string,
+    listener?: (event: TEvent, path: string) => void,
+  ): PathWatcher
+  export type TEvent = 'rename' | 'delete' | 'change'
+}
+import path = require('path')
+const pathWatcherPath = path.join(
+  atom.packages.resourcePath,
+  '/node_modules/pathwatcher/lib/main',
+)
+import _unused = require('pathwatcher')
+// tslint:disable-next-line:no-var-requires
+const { watch } = require(pathWatcherPath) as typeof _unused
+import PathWatcher = _unused.PathWatcher
+import TEvent = _unused.TEvent
 
 interface ImageRegisterRec {
   version: number
@@ -25,19 +43,22 @@ const refreshImages = _.debounce(async function(src: string) {
 }, 250)
 
 function srcClosure(src: string) {
-  return function(events: FilesystemChangeEvent) {
-    for (const event of events) {
-      const i = imageRegister[src]
-      if (!i) return
-      if (event.action === 'modified' && fs.isFileSync(src)) {
-        i.version = Date.now()
-      } else {
-        i.watcher.dispose()
-        delete imageRegister[src]
-      }
-      // tslint:disable-next-line: no-floating-promises
-      refreshImages(src)
+  // return function(events: FilesystemChangeEvent) {
+  return function(event: TEvent, path: string) {
+    // for (const event of events) {
+    console.log(event, path)
+    const i = imageRegister[src]
+    if (!i) return
+    if (event === 'change' && fs.isFileSync(src)) {
+      i.version = Date.now()
+    } else {
+      // i.watcher.dispose()
+      i.watcher.close()
+      delete imageRegister[src]
     }
+    // tslint:disable-next-line: no-floating-promises
+    refreshImages(src)
+    // }
   }
 }
 
@@ -48,7 +69,8 @@ export function removeFile(file: string) {
     image.files = _.filter(image.files, fs.isFileSync)
     if (_.isEmpty(image.files)) {
       image.watched = false
-      image.watcher.dispose()
+      // image.watcher.dispose()
+      image.watcher.close()
     }
     return image
   })
@@ -65,7 +87,8 @@ export async function getVersion(image: string, file?: string) {
         watched: true,
         files: file ? [file] : [],
         version,
-        watcher: await watchPath(image, {}, srcClosure(image)),
+        // watcher: await watchPath(image, {}, srcClosure(image)),
+        watcher: watch(image, srcClosure(image)),
       }
       return version
     } else {
