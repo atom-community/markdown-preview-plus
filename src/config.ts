41b6ac0885ac8e5b8c7196b18bd404b5cbfd7cf4
Nikolay Yakimov <root@livid.pp.ru>
2018-01-12 06:44:29 +0300

    Move config definiton into config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const config = {
   previewSplitPaneDir: {
     title: 'Direction to load the preview in split pane',
     type: 'string',
-    default: 'right',
+    default: 'right' as 'right' | 'down',
     enum: ['down', 'right'],
     order: 25,
   },
@@ -32,6 +32,9 @@ export const config = {
       'text.plain.null-grammar',
     ],
     order: 30,
+    items: {
+      type: 'string',
+    },
   },
   enableLatexRenderingByDefault: {
     title: 'Enable Math Rendering By Default',
@@ -76,21 +79,27 @@ Pandoc parser`,
   },
   pandocFilters: {
     type: 'array',
-    default: [],
+    default: [] as string[],
     title: 'Pandoc Options: Filters',
     description:
       'Comma separated pandoc filters, in order of application. Will be passed via command-line arguments',
     dependencies: ['enablePandoc'],
     order: 115,
+    items: {
+      type: 'string',
+    },
   },
   pandocArguments: {
     type: 'array',
-    default: [],
+    default: [] as string[],
     title: 'Pandoc Options: Commandline Arguments',
     description:
       'Comma separated pandoc arguments e.g. `--smart, --filter=/bin/exe`. Please use long argument names.',
     dependencies: ['enablePandoc'],
     order: 120,
+    items: {
+      type: 'string',
+    },
   },
   pandocMarkdownFlavor: {
     type: 'string',
@@ -152,4 +161,51 @@ Filters, but before other commandline arguments\
     dependencies: ['pandocBibliography'],
     order: 175,
   },
+}
+
+declare module 'atom' {
+  interface ConfigValues {
+    'markdown-preview-plus.breakOnSingleNewline': boolean
+    'markdown-preview-plus.liveUpdate': boolean
+    'markdown-preview-plus.openPreviewInSplitPane': boolean
+    'markdown-preview-plus.previewSplitPaneDir': 'down' | 'right'
+    'markdown-preview-plus.grammars': string[]
+    'markdown-preview-plus.enableLatexRenderingByDefault': boolean
+    'markdown-preview-plus.useLazyHeaders': boolean
+    'markdown-preview-plus.useGitHubStyle': boolean
+    'markdown-preview-plus.enablePandoc': boolean
+    'markdown-preview-plus.useNativePandocCodeStyles': boolean
+    'markdown-preview-plus.pandocPath': string
+    'markdown-preview-plus.pandocFilters': string[]
+    'markdown-preview-plus.pandocArguments': string[]
+    'markdown-preview-plus.pandocMarkdownFlavor': string
+    'markdown-preview-plus.pandocBibliography': boolean
+    'markdown-preview-plus.pandocRemoveReferences': boolean
+    'markdown-preview-plus.pandocBIBFile': string
+    'markdown-preview-plus.pandocBIBFileFallback': string
+    'markdown-preview-plus.pandocCSLFile': string
+    'markdown-preview-plus.pandocCSLFileFallback': string
+    'markdown-preview-plus': {
+      breakOnSingleNewline: boolean
+      liveUpdate: boolean
+      openPreviewInSplitPane: boolean
+      previewSplitPaneDir: 'down' | 'right'
+      grammars: string[]
+      enableLatexRenderingByDefault: boolean
+      useLazyHeaders: boolean
+      useGitHubStyle: boolean
+      enablePandoc: boolean
+      useNativePandocCodeStyles: boolean
+      pandocPath: string
+      pandocFilters: string[]
+      pandocArguments: string[]
+      pandocMarkdownFlavor: string
+      pandocBibliography: boolean
+      pandocRemoveReferences: boolean
+      pandocBIBFile: string
+      pandocBIBFileFallback: string
+      pandocCSLFile: string
+      pandocCSLFileFallback: string
+    }
+  }
 }
