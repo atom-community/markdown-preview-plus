2ab7b7c02ed0a4841e14dd033e00b2167fcd9770
Nikolay Yakimov <root@livid.pp.ru>
2018-07-13 23:37:00 +0300

    Reduce dependency on lodash

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,3 +1,9 @@
 interface Function {
   bind<T>(this: T, thisArg: any): T
 }
+
+interface ObjectConstructor {
+  entries<T extends object>(
+    x: T,
+  ): Array<{ [K in keyof T]-?: [K, T[K]] }[keyof T]>
+}
