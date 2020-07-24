6ea1daca0f07781752d8a45e0cef1794b7a4f49c
Nikolay Yakimov <root@livid.pp.ru>
2020-07-24 14:07:01 +0300

    Rewrite tree-sitter compatible highlighter

--- src/types/atom.d.ts
+++ src/types/atom.d.ts
@@ -22,11 +22,29 @@ declare module 'atom' {
     }
   }
   interface TextBuffer {
-    getLanguageMode(): {
-      readonly fullyTokenized?: boolean
-      readonly tree?: boolean
-    }
+    getLanguageMode(): LanguageMode
+    setLanguageMode(m: LanguageMode): void
   }
+  interface GrammarRegistry {
+    grammarForId(id: string): Grammar
+    languageModeForGrammarAndBuffer(g: Grammar, b: TextBuffer): LanguageMode
+  }
+  interface LanguageMode {
+    readonly fullyTokenized?: boolean
+    readonly tree?: boolean
+    onDidTokenize(cb: () => void): Disposable
+    buildHighlightIterator(): HighlightIterator
+    classNameForScopeId(id: ScopeId): string
+    startTokenizing?(): void
+  }
+  interface HighlightIterator {
+    seek(pos: { row: number; column: number }): void
+    getPosition(): { row: number; column: number }
+    getOpenScopeIds?(): ScopeId[]
+    getCloseScopeIds?(): ScopeId[]
+    moveToSuccessor(): void
+  }
+  interface ScopeId {}
   interface TextEditorElement {
     setUpdatedSynchronously(val: boolean): void
   }
