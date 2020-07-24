72618476f483177e151ee2b1a64e163f93f11a6e
Nikolay Yakimov <root@livid.pp.ru>
2020-07-24 17:40:43 +0300

    Typed webworker IPC

---
+++ src-worker/ipc.d.ts
@@ -0,0 +1,14 @@
+import { ConfigValues } from 'atom'
+
+export type MessageToWorker =
+  | {
+      cmd: 'config'
+      arg: ConfigValues['markdown-preview-plus.markdownItConfig']
+    }
+  | { cmd: 'render'; text: string; rL: boolean; id: number }
+  | { cmd: 'getTokens'; text: string; rL: boolean; id: number }
+
+export type MessageFromWorker = 
+  | { evt: 'odd-separators'; arr: unknown[]; option: string }
+  | { cmd: 'getTokens'; id: number; result: string }
+  | { cmd: 'render'; id: number; result: string }
